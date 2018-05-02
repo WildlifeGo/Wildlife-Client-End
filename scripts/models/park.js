5c17e9dad05af2cf499c272904a953fa5c14f881
Timea Heidenreich <timea.heidenreich@gmail.com>
2018-05-02 15:29:41 -0700

    login setup

--- scripts/models/park.js
+++ scripts/models/park.js
@@ -9,7 +9,44 @@ ENV.productionApiUrl = 'insert cloud API server URL here';
 ENV.developmentApiUrl = 'http://localhost:3000';
 ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
 
+(function(module) {
 
+  $('.login-form').on('submit', (event) => {
+    event.preventDefault();
+    console.log(event.target);
+
+    let username = $('input[name="username"]').val();
+    let password = $('input[name="password"]').val();
+
+    console.log(username, password);
+
+    if (username === 'user' && password === '1234'){
+
+      console.log('hitting if statement');
+      
+      $('form').addClass('hide');
+      $('.sign-out-button').removeAttr('hidden');
+      //call said function here
+    }  
+  });
+
+  $('.sign-out-button').on('click', (event) => {
+    
+    console.log(event.target);
+    $('form').removeClass('hide');
+    $('.sign-out-button').prop('hidden', true);
+    $('form')[0].reset();
+  });
+
+  username.fetch = (username, callback) => {
+
+    $.get(`${ENV.apiUrl}/api/v1/sign-in`)
+    
+  }
+
+  module.login = login;
+  
+})(app);
 
 
 (function (module) {
@@ -34,7 +71,7 @@ ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
     Park.all.push(this);
   }
 
-  //This function will get called once a park button has been clicked on the homepage. It will query the iNaturalist API and return a raw data object that we will append to the that particular park object. Might need to pass in the Park index as well as the rawAnimals index. 
+  //This function will get called once a park button has been clicked on the homepage. It will query the iNaturalist API and return a raw data object that we will append to the that particular park object. Might need to pass in the Park index as well as the rawAnimals index.
 
   Park.loadAnimals = function (rawAnimalObj) {
     Park.all[rawAnimalObj[0].park].animals = rawAnimalObj;
@@ -50,7 +87,7 @@ ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
   };
 
   //Create instances for each park. Might not need to set to variables since they're being stored in Park.all anyway.
-  //TODO: Fill in park descriptions. 
+  //TODO: Fill in park descriptions.
   new Park('Discovery Park', 47.661817, -122.417857, 1, 'Discovery Park is a 534 acre natural area park operated by the Seattle Parks and Recreation. It is the largest city park in Seattle, and occupies most of the former Fort Lawton site. The site is one of breathtaking majesty. Situated on Magnolia Bluff overlooking Puget Sound, Discovery Park offers spectacular view of both the Cascade and the Olympic Mountain ranges. The secluded site includes two miles of protected tidal beaches as well as open meadow lands, dramatic sea cliffs, forest groves, active sand dunes, thickets and streams.', 'https://www.seattle.gov/images/Departments/ParksAndRecreation/Parks/DEF/DiscoveryPark5.jpg', 0);
   new Park('Washington Park Arboretum', 47.635974, -122.294531, 1, 'The Arboretum is a hidden gem on the shores of Lake Washington. Jointly managed by the University of Washington Botanic Gardens and the City of Seattle, its 230 acres contain a dynamic assortment of plants, some found nowhere else in the Northwest. Take a walk through the Arboretum and discover this beautiful living collection.', 'https://statesymbolsusa.org/sites/statesymbolsusa.org/files/Washington-arboretum-2.jpg', 1);
   new Park('Interlaken Park', 47.636529, -122.309307, 0.5, 'Interlaken Park is a densely wooded area on the north end of Capitol Hill. The paths and trails throughout the park are frequented by bikers, hikers and joggers.', 'https://www.seattle.gov/images/Departments/ParksAndRecreation/Parks/GHI/InterlakenPark2.jpg', 2);
