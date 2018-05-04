6f4aeb797fc5dcd323c3109b9e2acbc277565625
Rhiannon98 <mortensenrhi@gmail.com>
2018-05-04 10:26:48 -0700

    url fixes in route.js

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -2,7 +2,8 @@
 
 //TODO: I still don't understand how to implement page js very well. Go back and reference book code. Right now, I don't think this is actually doing anything.
 
-page('/' , () => app.parkView.initHomePage());
-  // , (ctx, next) => app.adminView.verify(ctx, next)
+page('/', () => app.parkView.initHomePage());
+// , (ctx, next) => app.adminView.verify(ctx, next)
+page('/client-wildlife', () => app.parkView.initHomePage());
 
 page();
