250c223f6ebebc38c65c0a089ea26a816c9f66e3
Rhiannon Mortensen <34196086+Rhiannon98@users.noreply.github.com>
2018-05-06 13:25:39 -0700

    Update routes.js
    zombie hoard killed.

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -1,9 +1,7 @@
 'use strict';
 
-//TODO: I still don't understand how to implement page js very well. Go back and reference book code. Right now, I don't think this is actually doing anything.
 
 page('/', () => app.parkView.initHomePage());
-// , (ctx, next) => app.adminView.verify(ctx, next)
 page('/client-wildlife', () => app.parkView.initHomePage());
 
 page();
