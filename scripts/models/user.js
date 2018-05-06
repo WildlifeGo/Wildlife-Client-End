fa2a332e67c520f5ffc69b6a31cc9cb20dcdc163
Rhiannon Mortensen <34196086+Rhiannon98@users.noreply.github.com>
2018-05-06 13:42:51 -0700

    Update user.js
    zombie hoard killed.

--- scripts/models/user.js
+++ scripts/models/user.js
@@ -10,22 +10,17 @@ KTM.developmentApiUrl = 'http://localhost:3000';
 KTM.apiUrl = KTM.isProduction ? KTM.productionApiUrl : KTM.developmentApiUrl;
 
 (function (module) {
-
   function errorCallback(err) {
-    console.error(err);
     module.errorView.initErrorPage(err);
   }
-
   function User(userObj) {
-
     Object.keys(userObj).forEach(key => this[key] = userObj[key]);
   }
-
   User.create = (user, callback) =>
     $.post(`${KTM.apiUrl}/api/v1/signin`, user)
       .then(callback)
       .catch(errorCallback);
-
+  
   module.User = User;
 
 })(app);
